d2dcb0dd2dc26228e5cd989a4f0b6babc9fc53ae
zagganas <zagganas@athenarc.gr>
2021-09-06 12:11:28 +0000

    Fixed a bug with machine VM deletion

--- views/project/vm_machines_details.php
+++ views/project/vm_machines_details.php
@@ -203,7 +203,7 @@ if (isset(Yii::$app->params['windowsImageIDs'][$model->image_id]))
 ?>
 
 
-<div class="modal fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="delete-modal" aria-hidden="true">
+<div class="modal delete fade" id="delete-modal" tabindex="-1" role="dialog" aria-labelledby="delete-modal" aria-hidden="true">
 	<div class="modal-dialog modal-dialog-centered" role="document">
 		<div class="modal-content">
    			<div class="modal-header">
