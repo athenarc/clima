a654bdfd108747c6526bbbce692c7a404d3e2348
Panagiotis Deligiannis <deligianp@gmail.com>
2022-06-01 16:12:16 +0300

    Refine load indicator bars to higher percentage resolution
    - Remove complexity of calculating width for small percentages. Instead,
     enforce a minimal 2px width for bars that correspond to non-zero values
     .

--- components/LoadIndicator.php
+++ components/LoadIndicator.php
@@ -35,32 +35,16 @@ class LoadIndicator extends Widget
 
     public function run()
     {
-        $currentRatio = $this->current / $this->total;
-
-        /**
-         * The core idea here is that at all times, the rendered widget should inform the user whether more load is
-         * requested, regardless of the amount of it. Even for cases where the load is infinitesimal, a minimum-width
-         * segment should be rendered to imply this information.
-         *
-         * At this point the minimum width is set to 1% of the load indicator. To account for this normalization,
-         * the segment for the current load will always change accordingly:
-         * - if current load is more than 99% and there is an amount of requested load, current-bar's width, will
-         * drop to 99%, in order to accommodate for the requested-bar's normalization.
-         * - if current load is infinitesimal as well (current_load<1%), then the same normalization for the
-         * current-bar as well; both current-bar and requested-bar will have the minimum width of 1%.
-         */
-        $currentBarDrawRatio = $currentRatio < 0.99 ? ceil($currentRatio * 100) : floor($currentRatio * 100);
-
-        $requestedBarDrawRatio = min(ceil(($this->requested / $this->total) * 100), 100 - $currentBarDrawRatio);
-
-        $remainingBarDrawRatio = 100 - $currentBarDrawRatio - $requestedBarDrawRatio;
+        $currentRatio = 100 * ($this->isPositive? $this->current : $this->current+$this->requested) / $this->total;
+        $requestedRatio = 100 * abs($this->requested)/$this->total;
+        $remainingRatio = 100 - $currentRatio - $requestedRatio;
 
         return $this->render(
             'load-indicator',
             [
-                'current' => $currentBarDrawRatio,
-                'requested' => $requestedBarDrawRatio,
-                'remaining' => $remainingBarDrawRatio,
+                'current' => $currentRatio,
+                'requested' => $requestedRatio,
+                'remaining' => $remainingRatio,
                 'exceeding' => $this->exceeding,
                 'currentLabel' => $this->currentLabel,
                 'requestedLabel' => $this->requestedLabel,
