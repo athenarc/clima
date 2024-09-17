490294fb20dd53a435f2775bf36fda06b018a944
EiriniGeorgiaDimitriou <dim.georgian@gmail.com>
2024-09-17 07:51:34 +0000

    First commit

--- controllers/ProjectController.php
+++ controllers/ProjectController.php
@@ -78,6 +78,19 @@ class ProjectController extends Controller
         ];
     }
 
+    public function beforeAction($action)
+    {
+        if (!parent::beforeAction($action)) {
+            return false;
+        }
+
+        // Set the flash message
+        Yii::$app->session->setFlash('danger', 'Dear HYPATIA users,</p><p>We would like to inform you that we are aware of a major issue on the infrastructure causing problems during the allocation and connection to resources. We are currently investigating the issue and trying to resolve it as soon as possible.</p><p>HYPATIA support team</p>');
+
+        // Continue with the action
+        return true;
+    }
+
     /**
      * {@inheritdoc}
      */
